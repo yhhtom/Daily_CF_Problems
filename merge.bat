f2f36472a24ae87d0b8e1a9332e86dd61811ddf8
PD20W <yuanhaohang@outlook.com>
2026-03-17 09:30:45 +0800

    Auto-save before sync

--- merge.bat
+++ merge.bat
@@ -1,25 +1,11 @@
-
 git add -A
-
 git commit -m "Auto-save before sync"
-
-
 git checkout main
-
 git pull upstream
-
-
 git checkout practice
-
 git merge main
-
 git checkout practice
-
-
 git add -A
-
 git commit -m "Daily submission: merged upstream updates"
-
 git push origin practice
-
 pause
