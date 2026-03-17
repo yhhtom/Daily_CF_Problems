365311af924b495b41920b12678b43a53b80012f
PD20W <yuanhaohang@outlook.com>
2026-03-17 09:33:57 +0800

    Auto-save before sync

--- merge.bat
+++ merge.bat
@@ -1,12 +1,12 @@
 chcp 65001 > nul
-git add -A
-git commit -m "Auto-save before sync"
-git checkout main
-git pull upstream
-git checkout practice
-git merge main
-git checkout practice
-git add -A
-git commit -m "Daily submission: merged upstream updates"
-git push origin practice
+call git add -A
+call git commit -m "Auto-save before sync"
+call git checkout main
+call git pull upstream
+call git checkout practice
+call git merge main
+call git checkout practice
+call git add -A
+call git commit -m "Daily submission: merged upstream updates"
+call git push origin practice
 pause
